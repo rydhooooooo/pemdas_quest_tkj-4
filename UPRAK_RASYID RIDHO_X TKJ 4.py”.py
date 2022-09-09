882bae799cb75bdd1ddb142b1ab7a86b21d25dc8
rydhooooooo <112526293+rydhooooooo@users.noreply.github.com>
2022-09-09 14:54:18 +0700

    Add files via upload

---
+++ UPRAK_RASYID RIDHO_X TKJ 4.py”.py
@@ -0,0 +1,44 @@
+# rumus volume balok panjang * lebar * tinggi
+
+panjang=15
+lebar=20
+tinggi=5
+
+volume_b = panjang * lebar * tinggi
+print("volume balok adalah", volume_b)
+
+# rumus volume kubus sisi * sisi * sisi
+
+sisi = 10
+sisi = 10
+sisi = 10
+
+volume_k = sisi * sisi * sisi
+print("volume kubus adalah", volume_k)
+
+# rumus limas 1/3 * luas alas * tinggi
+
+a = 15
+t = 25
+luas_l =  a * t /3
+print("rumus luas permukaan limas adalah", luas_l)
+
+# rumus  luas tabung 22/7 x r x r x t
+
+r = 8
+r = 8
+t = 20
+
+luas_t = r * r * t /7
+print("rumus luas tabung adalah", luas_t)
+
+
+# celcius to reamur celcius * 4/5
+
+celcius = 20
+reamur = 15
+suhu = 20 * 4/5
+
+print("celcius dijadikan reamur",suhu)
+
+
