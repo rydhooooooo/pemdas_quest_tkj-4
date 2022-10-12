55a9f125fec1af7237d20f7d542beb6254542868
rydhooooooo <112526293+rydhooooooo@users.noreply.github.com>
2022-10-12 14:05:45 +0700

    Add files via upload

---
+++ python10.2-tkj4.py
@@ -0,0 +1,70 @@
+# membuat tipe data koleksi
+
+koleksi_data_str = ["pisang","mangga","semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["rizky billar", 100, True, "reza arap"]
+
+print("koleksi data string:", koleksi_data_str) 
+
+print("koleksi data interger:", koleksi_data_int)
+
+print("koleksi data campuran:", koleksi_data_mix)
+
+koleksi_nama_hewan = ["macan","kancil","gajah","semut"]
+
+koleksi_data_int = [100, 100, 100, 100]
+
+koleksi_nama_teman = ["naufal","fadlan","jamil","rafi"]
+
+print("koleksi data string:", koleksi_nama_hewan) 
+
+print("koleksi data interger:", koleksi_data_int)
+
+print("koleksi data string:", koleksi_nama_teman)
+
+# tampilkan data koleksi dengan indeks 
+
+# data ke 2 nama hewan
+
+print("nama hewan data ke-2:", koleksi_nama_hewan[1])
+
+# data pertama nilai uts
+
+print("nilai uts data ke-1:", koleksi_data_int[0])
+
+# data terakhir nama teman sebangku
+
+print("nama teman sebangku ke-4:", koleksi_nama_teman[3])
+
+# tambahkan 1 data disetiap data koleksi
+
+koleksi_nama_teman = ["naufal","fadlan","jamil","rafi"]
+
+print(koleksi_nama_teman)
+
+koleksi_nama_teman.append("ade ytta")
+
+print(koleksi_nama_teman)
+
+# ubahlah data terakhir nilai UTS
+
+koleksi_data_int[-1] = "99"
+
+print(koleksi_data_int)
+
+#ubahlah data ke-3 nama hewan
+
+koleksi_nama_hewan[2] = "singa"
+
+print(koleksi_nama_hewan)
+
+                          
+
+
+
+      
+
+
+
